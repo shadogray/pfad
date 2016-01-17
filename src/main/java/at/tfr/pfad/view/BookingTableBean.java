6e1661ad47d2b726735b697a97a991c86a671f70
ThomasF <fruehbeck@aon.at>
2016-01-17 15:49:26 +0100

    Copyright

--- src/main/java/at/tfr/pfad/view/BookingTableBean.java
+++ src/main/java/at/tfr/pfad/view/BookingTableBean.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2015 Thomas Frühbeck, fruehbeck(at)aon(dot)at.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package at.tfr.pfad.view;
 
 import java.io.Serializable;
