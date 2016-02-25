f9d2f33ef8cb001388b0c6ffa20c9296c8eb5c56
ThomasF <fruehbeck@aon.at>
2016-02-25 19:57:36 +0100

    remove duplication of not null

--- src/main/java/at/tfr/pfad/model/Configuration.java
+++ src/main/java/at/tfr/pfad/model/Configuration.java
@@ -48,7 +48,7 @@ public class Configuration implements PrimaryKeyHolder, Serializable {
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
-	@Column(nullable=false, columnDefinition="varchar2(16) default 'simple' not null")
+	@Column(nullable=false, columnDefinition="varchar2(16) default 'simple'")
 	@Enumerated(EnumType.STRING)
 	private ConfigurationType type;
 
