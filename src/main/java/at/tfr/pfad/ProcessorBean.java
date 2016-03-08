5d05074b011d15bfecec11852fa4ac89cb1f2fe3
ThomasF <fruehbeck@aon.at>
2016-03-08 03:03:10 +0100

    workable payment

--- src/main/java/at/tfr/pfad/ProcessorBean.java
+++ src/main/java/at/tfr/pfad/ProcessorBean.java
@@ -6,6 +6,7 @@ import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.jboss.logging.Logger;
 import org.joda.time.DateTime;
@@ -13,7 +14,7 @@ import org.joda.time.DateTime;
 @Singleton
 public class ProcessorBean implements Serializable {
 
-	@Inject
+	@PersistenceContext(unitName = "pfad")
 	private EntityManager entityManager;
 	private Logger log = Logger.getLogger(getClass());
 
