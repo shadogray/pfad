0cd2087948a343e8e0ecdcb6f3218ed7dce6fed1
ThomasF <fruehbeck@aon.at>
2016-03-13 19:19:22 +0100

    bad caching of bookings

--- src/main/java/at/tfr/pfad/view/PaymentBean.java
+++ src/main/java/at/tfr/pfad/view/PaymentBean.java
@@ -73,13 +73,11 @@ public class PaymentBean extends BaseBean implements Serializable {
 		if (this.id == null) {
 			this.payment = this.paymentExample;
 		} else {
-			if (payment == null || payment.getId() == null || !payment.getId().equals(id)) {
-				payment = findById(getId());
-				payment.getBookings().stream().findFirst().ifPresent(b -> payment.updateType(b.getActivity()));
-				paymentPayer = payment.getPayer();
-				if (payment.getPayer() != null) {
-					filteredPayers.add(payment.getPayer());
-				}
+			payment = findById(getId());
+			payment.getBookings().stream().findFirst().ifPresent(b -> payment.updateType(b.getActivity()));
+			paymentPayer = payment.getPayer();
+			if (payment.getPayer() != null) {
+				filteredPayers.add(payment.getPayer());
 			}
 		}
 	}
