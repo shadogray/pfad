107ff6bc614b442c5ec7d9c80b1176f832a34330
ThomasF <fruehbeck@aon.at>
2016-04-10 19:11:13 +0200

    create indexes

--- doc/pfad.sql
+++ doc/pfad.sql
@@ -10,3 +10,11 @@ alter table payment alter column version set default 0;
 alter table member alter column version set default 0;
 alter table squad alter column version set default 0;
 alter table function alter column version set default 0;
+
+create index booking_has_activity_idx on booking (activity_id);
+create index booking_has_member_idx on booking (member_id);
+create index booking_has_activity_and_menber_idx on booking (activity_id, member_id);
+create index member_has_trupp on member (trupp_id);
+create index member_has_vollzahler on member (vollzahler_id);
+create index member_has_function on member (funktionen_id);
+create index payment_has_payer on payment (payer_id);
