6e2b1d7e78e33db8489b3a0f58ca4967fcbf18b7
Frühbeck Thomas <thomas.fruehbeck@a1telekom.at>
2018-05-24 13:15:49 +0200

    update date pattern

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,5 +1,7 @@
 pushd ./pfad-master/ || exit 1
 mvn clean install || exit 1
 popd
-scp ./pfad-master/pfad/target/pfad.war laappsms001:./deploy/pfad.war
-ssh laappsms001 mv ./deploy/pfad.war /opt/wildfly/wildfly101/standalone/deployments/pfad.war
+#scp ./pfad-master/pfad/target/pfad.war laappsms001:./deploy/pfad.war
+#ssh laappsms001 mv ./deploy/pfad.war /opt/wildfly/wildfly101/standalone/deployments/pfad.war
+scp ./pfad-master/pfad/target/pfad.war vlproap001.at.inside:./deploy/pfad.war
+ssh vlproap001.at.inside mv ./deploy/pfad.war /opt/jboss/pfad/wildfly/standalone/deployments/pfad.war
