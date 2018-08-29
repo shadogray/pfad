73ab1767bdefa0c7f4f1777f9fccf34d059d1aa1
Frühbeck Thomas <thomas.fruehbeck@a1telekom.at>
2018-08-29 11:05:37 +0200

    document changes via SQL

---
+++ doc/changes.sql
@@ -0,0 +1,12 @@
+2018.08.29: Anmeldungen, die nach Aufnahme nicht im Status 'Mitglied' waren:
+
+select r.id, m.id, r.name, r.vorname, r.member_id, m.id, m.name, m.vorname
+    from Registration r inner join Member m on r.name = m.name and r.vorname = m.vorname
+where r.status = 'Erstellt'
+and m.id = r.member_id;
+
+--update Registration set status = 'Mitglied' where id in (
+--    select r.id from Registration r inner join Member m on r.name = m.name and r.vorname = m.vorname
+--    where r.status = 'Erstellt'  and m.id = r.member_id
+--);
+
