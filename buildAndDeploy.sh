7883161a9aa9f03a10dbcb691e2c3c6196f68d1e
u0x27vo <thomas.fruehbeck@a1.at>
2019-08-05 12:21:27 +0200

    add sleep

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -5,5 +5,5 @@ popd
 #ssh laappsms001 mv ./deploy/pfad.war /opt/wildfly/wildfly101/standalone/deployments/pfad.war
 scp ./pfad-master/pfad/target/pfad.war vlproap001.at.inside:/opt/jboss/pfad/pfad.war
 ssh vlproap001.at.inside rm -f /opt/jboss/pfad/wildfly/standalone/deployments/pfad.war
-#sleep 5
+sleep 5
 ssh vlproap001.at.inside cp /opt/jboss/pfad/pfad.war /opt/jboss/pfad/wildfly/standalone/deployments/pfad.war
