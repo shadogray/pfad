e005310fbf1ec77ffaa4a71b896d8b48928654c5
u0x27vo <thomas.fruehbeck@a1.at>
2019-09-03 17:13:11 +0200

    simplify CC/BCC handling

--- pfad-master/pfad/src/main/java/at/tfr/pfad/view/MailerBean.java
+++ pfad-master/pfad/src/main/java/at/tfr/pfad/view/MailerBean.java
@@ -154,6 +154,7 @@ public class MailerBean extends BaseBean {
 						String join = e.getValue().stream()
 								.map(line -> (String)line.get(ccIdx).getValue())
 								.filter(StringUtils::isNotBlank)
+								.filter(v -> !v.equalsIgnoreCase(e.getKey()))
 								.distinct()
 								.collect(Collectors.joining(","));
 						e.getValue().get(0).get(ccIdx).setValue(join);
